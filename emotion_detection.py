a065b67c7dcbf81df337d273b2c3decdf84d4d91
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-06-24 17:57:06 -0400

    res from emotion_detector function in emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,15 +1,18 @@
 """
-The following module provides a function that grades speech
+The following module provides a function that reads a string
+and Outputs the likelihood of an emotion based off the string
 """
 
 # Import of the json library to help with formatting
 import json
 # Import of the requests library to handle HTTP requests
-import requests 
+import requests
+
 
 def emotion_detector(text_to_analyze):
     """
     Takes a string as input
+    Outputs a responsed predicting likelihood of an emmotion
     """
     # Url of Emotion Predict service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -19,7 +22,5 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document":{"text":text_to_analyze}}
     # Post request to the API using heads and text
     response = requests.post(url=url,json=myobj,headers=header)
-    return response
-# URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-# Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-# Input json: { "raw_document": { "text": text_to_analyze } }
+    # Response text from the URL
+    return json.loads(response.text)
